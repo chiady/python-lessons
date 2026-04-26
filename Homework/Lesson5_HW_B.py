1ed82c1b0a32573c22e2cfb4a4e49e227dfafa01
chiady <154931996+chiady@users.noreply.github.com>
2026-04-26 08:41:13 -0700

    new feature - write image

--- Homework/Lesson5_HW_B.py
+++ Homework/Lesson5_HW_B.py
@@ -11,12 +11,16 @@ parser = argparse.ArgumentParser(description="My Homework Script")
 parser.add_argument("-f", "--file", help="The image file to process", required=True)
 # Optional argument with a default value
 parser.add_argument("-r", "--read_image_number", default=0, help="The image number to read from the TIFF stack (default: 0)")
+parser.add_argument("-o", "--output_file", default=None, help="If provided, will write the image selected into a output file")
+
 args = parser.parse_args()
 
 image_file = args.file
 print(f"Processing: {image_file}")
 read_image_number = int(args.read_image_number)
 print(f"Reading image number: {read_image_number}")
+output_file = args.output_file
+print(f"Writing Image to file: {output_file}")
 
 # Example for single imege reading (JPG, PNG, etc.)
 #img = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
@@ -39,7 +43,13 @@ if success:
         image_x = images[x]
         plt.imshow(image_x, cmap='gray')
         plt.show()
+        if output_file is not None:
+            # Save the image to a file
+            cv2.imwrite(output_file, image_x)
+
     else:
         print(f"Index {x} is out of range. Valid range is 0 to {len(images)-1}.")
 else:
     print("Failed to read the TIFF stack.")
+
+
